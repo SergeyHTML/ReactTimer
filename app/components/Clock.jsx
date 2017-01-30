abf8b48e22bcdd634a883a97026f0b9c3465e25e
Sergey <dubich.sv@gmail.com>
2017-01-30 22:39:50 +0200

    Add automatic tests

---
+++ app/components/Clock.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+vat Clock = React.createClass({
+  render: function () {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+});
+
+module.exports = Clock;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var Timer = (props) => {
+    return(
+        <div>
+            <p className="">Timer.jsx</p>
+
+        </div>
+    )
+}
+
+module.exports = Timer;
